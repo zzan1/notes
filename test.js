1255ecfc9cf2ad26e2a6b1cf93f7895d77ce89c1
zzan1 <2448678836@qq.com>
2020-06-03 23:27:55 +0800

    i

--- test.js
+++ test.js
@@ -1,17 +1,21 @@
-var book = {
-	_year: 2004,
-	edition: 1
-};
+const numbers=[1,2,3,4,5]
 
-Object.defineProperties(book,{
-	"year":{
-		"value": 2005
-	},
-	"edition":{
-		"value": 3
-	}
-})
+let max=Math.max.apply(Math, numbers)
 
-console.log(book.year);
+// console.log(Math.random()*9 + 10)
 
-console.log(book.edition);
+const IF=false;
+
+const IFObj=new Boolean(false)
+
+const number = 100.12;
+
+//console.log(number.toString(), number.toLocaleString(), number.valueOf())
+// console.log(number.toString(2), number.toString(16))
+// console.log(number.toFixed(1), number.toExponential(1), number.toPrecision(1))
+//
+const strT = "this我"
+console.log(strT.length)
+console.log(strT.charAt(4), strT.charCodeAt(4))
+const strT2="+12bulue"
+console.log(parseInt(strT2))
